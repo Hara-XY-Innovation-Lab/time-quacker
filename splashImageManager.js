b9404d2b80058b125fac6b001df9446c00eb9971
Karthikpandala <karthikpandala0502@gmail.com>
2025-07-23 22:16:33 +0530

    Refactored main.js into: windows, tray, camera, detection, and splash image logic

---
+++ splashImageManager.js
@@ -0,0 +1,28 @@
+const fs = require('fs');
+const path = require('path');
+
+const splashDir = path.join(__dirname, 'Assets', 'splash');
+let splashShufflePool = [];
+
+function getSplashImages() {
+  return fs.existsSync(splashDir)
+    ? fs.readdirSync(splashDir).filter(f => /\.(png|jpe?g|gif|bmp|webp)$/i.test(f))
+    : [];
+}
+
+function getRandomSplashImage() {
+  if (splashShufflePool.length === 0) {
+    const allImages = getSplashImages();
+    for (let i = allImages.length - 1; i > 0; i--) {
+      const j = Math.floor(Math.random() * (i + 1));
+      [allImages[i], allImages[j]] = [allImages[j], allImages[i]];
+    }
+    splashShufflePool = allImages;
+  }
+
+  const chosen = splashShufflePool.pop();
+  if (!chosen) return null;
+  return 'file://' + path.join(splashDir, chosen).replace(/\\/g, '/');
+}
+
+module.exports = { getRandomSplashImage };
---
+++ windows.js
@@ -0,0 +1,46 @@
+const { BrowserWindow } = require('electron');
+const path = require('path');
+const { getRandomSplashImage } = require('./splashImageManager');
+
+function createSplashWindow() {
+  const splashImage = getRandomSplashImage();
+  const splashWindow = new BrowserWindow({
+    width: 480,
+    height: 320,
+    frame: false,
+    alwaysOnTop: true,
+    transparent: true,
+    resizable: false,
+    show: true,
+    webPreferences: { preload: path.join(__dirname, 'preload.js') }
+  });
+  splashWindow.loadFile(path.join('renderer', 'splash.html'), { query: { img: splashImage } });
+  return splashWindow;
+}
+
+function createMainWindow() {
+  const mainWindow = new BrowserWindow({
+    width: 800,
+    height: 600,
+    show: false,
+    webPreferences: {
+      preload: path.join(__dirname, 'preload.js'),
+      contextIsolation: true,
+      nodeIntegration: false,
+      backgroundThrottling: false
+    }
+  });
+
+  mainWindow.loadFile(path.join('renderer', 'index.html'));
+
+  mainWindow.on('close', (event) => {
+    if (!app.isQuiting) {
+      event.preventDefault();
+      mainWindow.hide();
+    }
+  });
+
+  return mainWindow;
+}
+
+module.exports = { createSplashWindow, createMainWindow };
